3f2b692479ece901f3f05e766bf207fdf4a7e480
Cody Reandeau <codyreandeau@yahoo.com>
2016-10-07 15:27:36 -0400

    Added a javascript file with some jquery. Created function: when scrolling the nav bar moves closer to the top of the screen.

---
+++ MyWebsite.js
@@ -0,0 +1,21 @@
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > 10) {
+        if (!$('#nav_wrapper').hasClass('shrink')) {
+            $('#nav_wrapper').addClass('shrink');
+        }
+    } else {
+        if ($('#nav_wrapper').hasClass('shrink')) {
+            $('#nav_wrapper').removeClass('shrink');
+        }
+    }
+
+     if ($(this).scrollTop() > 10) {
+        if (!$('#header_wrapper').hasClass('shrink')) {
+            $('#header_wrapper').addClass('shrink');
+        }
+    } else {
+        if ($('#header_wrapper').hasClass('shrink')) {
+            $('#header_wrapper').removeClass('shrink');
+        }
+    }
+});
